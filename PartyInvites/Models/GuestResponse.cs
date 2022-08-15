2bffc267ca4df9df06f9a2001cf24cb5614c002d
Biswadeep Dey <biswa8998@gmail.com>
2022-08-15 22:31:50 +0530

    Pro-Dotnet: Party Invites Project

---
+++ PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email address")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter your phone")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Please specify whether you'll attend or not")]
+        public bool? WillAttend { get; set; }
+    }
+}
